57c0b41548d22df84413e9fb527ec6d34aa1aa30
Mario <msiu001@fiu.edu>
2018-03-16 14:45:28 -0400

    ASP.NET MVC User Registration Form with Entity Framework and SQL

---
+++ UserRegistrationEF/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistrationEF.Models;
+
+namespace UserRegistrationEF.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit( int id=0 )
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DBModels dbModel = new DBModels())
+            {
+                if(dbModel.Users.Any(x => x.Username == userModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exits";
+                    return View("AddOrEdit", userModel);
+                }
+
+
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User());
+        }
+    }
+}
